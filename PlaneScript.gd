3832ff3f27bb543705eda54dbc180ab3e5fa04fb
baalmeida <bj.aa.414@gmail.com>
2020-11-12 15:42:29 -0600

    Added toon shader and omnilight (sun)

--- PlaneScript.gd
+++ PlaneScript.gd
@@ -3,6 +3,7 @@ extends RigidBody
 const v3 = Vector3(0, 0, 0)
 const scalar_z = Vector3(0, 0, -1)
 var force = v3
+var torque = v3
 var drag = v3
 var lift = v3
 var thrust = v3
@@ -10,11 +11,11 @@ var thrust = v3
 var bullet_scene = preload("Bullet.tscn")
 
 
-const MAX_THRUST = 500
+const MAX_THRUST = 60
 const MAX_CAM_ANGLE = 30
 const DRAG_CONST = 1000
 
-func calc_force(thrust, rotate):
+func calc_force(thrust, pitch):
     var speed = MAX_THRUST * (thrust)
     var drag_coef = DRAG_CONST / speed / speed 
     thrust = transform.basis.z * (-speed)
@@ -22,7 +23,12 @@ func calc_force(thrust, rotate):
     lift = transform.basis.y * drag_coef
     
     force = thrust + drag + lift
+    torque = transform.basis.x * pitch
+
+#    torque = transform.basis.y * w.z * rudder + transform.basis.z * w.z * ailerons - transform.basis.x * w.z * elevator
+func _integrate_forces(state):
     add_central_force(force)
+    add_torque(torque)
 
 func fire_weapon():
     var clone = bullet_scene.instance()
--- player.gd
+++ player.gd
@@ -1,23 +1,22 @@
 extends Node
 const MAX_CAM_ANGLE = 30
-var rotate = 0
+var pitch = 0
 var thrust = 0.5
 
 func _physics_process(delta):
-    if Input.is_action_pressed("fire"):
+    if Input.is_action_pressed("p1_fire"):
         $Plane.fire_weapon()
     if Input.is_action_pressed("ui_up"):
         thrust += delta
     if Input.is_action_pressed("ui_down"):
         thrust -= delta
-    if Input.is_action_pressed("ui_left"):
-        rotate -= delta
-    if Input.is_action_pressed("ui_right"):
-        rotate += delta
+    if Input.is_action_pressed("p1_pìtch_up"):
+        pitch += delta
+    if Input.is_action_pressed("p1_pitch_down"):
+        pitch -= delta
     
     thrust = clamp(thrust, 0.2, 1)
-    rotate = clamp(rotate, -1, 1)
-    $Plane.calc_force(thrust, rotate)  
+    $Plane.calc_force(thrust, pitch)  
 #	if Input.is_action_pressed("ui_rudder_left"):
 #		rudder -= d
 #	if Input.is_action_pressed("ui_rudder_right"):
