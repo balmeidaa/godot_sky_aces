8e0538ef82ccaaaca98b84377a280d6162590e46
mr1337357 <miss@papaya>
2021-06-14 23:36:16 -0700

    had tabs instead of spaced. my bad

--- PlaneScript.gd
+++ PlaneScript.gd
@@ -16,29 +16,29 @@ const MAX_CAM_ANGLE = 30
 const DRAG_CONST = 1
 
 func calc_force(thrust, pitch_dir, roll_dir, yaw_dir):
-	var speed = MAX_THRUST * (thrust)
-	var drag_coef = DRAG_CONST / MAX_THRUST / MAX_THRUST  
-	thrust = transform.basis.z * (-speed)
-	drag = transform.basis.z * drag_coef
-	lift = transform.basis.y * drag_coef
-	
-	force = thrust + drag + lift
-	var pitch = global_transform.basis.x * pitch_dir * MAX_THRUST_TURN
-	var yaw = global_transform.basis.z * yaw_dir * MAX_THRUST_TURN
-	var roll = global_transform.basis.z * roll_dir * MAX_THRUST_TURN
-	torque = pitch + yaw + roll
+    var speed = MAX_THRUST * (thrust)
+    var drag_coef = DRAG_CONST / MAX_THRUST / MAX_THRUST  
+    thrust = transform.basis.z * (-speed)
+    drag = transform.basis.z * drag_coef
+    lift = transform.basis.y * drag_coef
+    
+    force = thrust + drag + lift
+    var pitch = global_transform.basis.x * pitch_dir * MAX_THRUST_TURN
+    var yaw = global_transform.basis.z * yaw_dir * MAX_THRUST_TURN
+    var roll = global_transform.basis.z * roll_dir * MAX_THRUST_TURN
+    torque = pitch + yaw + roll
 
 #    torque = transform.basis.y * w.z * rudder + transform.basis.z * w.z * ailerons - transform.basis.x * w.z * elevator
 func _integrate_forces(state):
-	add_central_force(force)
-	#add_torque(torque)
-	angular_velocity = torque
+    add_central_force(force)
+    #add_torque(torque)
+    angular_velocity = torque
 
 func fire_weapon():
-	var clone = bullet_scene.instance()
-	var scene_root = get_tree().root.get_children()[0]
-	scene_root.add_child(clone)
-	
-	clone.global_transform = $Gunpoint.global_transform
+    var clone = bullet_scene.instance()
+    var scene_root = get_tree().root.get_children()[0]
+    scene_root.add_child(clone)
+    
+    clone.global_transform = $Gunpoint.global_transform
 #    clone.BULLET_DAMAGE = DAMAGE
 
--- player.gd
+++ player.gd
@@ -6,54 +6,54 @@ var roll_dir = 0
 var thrust = 0.5
 var input = ''
 func _ready():
-	$DebugStats.add_property($Plane, "transform:origin", "round")
-	$DebugStats.add_property($Plane, "force", "round")
-	$DebugStats.add_property($Plane, "torque", "round")
-	$DebugStats.add_property(self, "pitch_dir", "round")
-	$DebugStats.add_property(self, "input", "")
+    $DebugStats.add_property($Plane, "transform:origin", "round")
+    $DebugStats.add_property($Plane, "force", "round")
+    $DebugStats.add_property($Plane, "torque", "round")
+    $DebugStats.add_property(self, "pitch_dir", "round")
+    $DebugStats.add_property(self, "input", "")
 
 func _physics_process(delta):
-	process_inputs(delta)
-	process_movement(delta)
-	
+    process_inputs(delta)
+    process_movement(delta)
+    
 func process_inputs(delta):
-	pitch_dir = pitch_dir / (1 + delta)
-	roll_dir = roll_dir / (1 + delta)
-	if Input.is_action_pressed("p1_fire"):
-		$Plane.fire_weapon()
-		input = 'fire'
-	if Input.is_action_pressed("ui_up"):
-		thrust += delta
-		input = 'accel'
-	if Input.is_action_pressed("ui_down"):
-		thrust -= delta
-		input = 'slow'
-	if Input.is_action_pressed("p1_pitch_up"):
-		pitch_dir += delta
-		input = 'up'
-	if Input.is_action_pressed("p1_pitch_down"):
-		pitch_dir -= delta
-		input = 'down'
-	if Input.is_action_pressed("p1_roll_left"):
-		roll_dir += delta
-		input = 'yaw left'
-	if Input.is_action_pressed("p1_roll_right"):
-		roll_dir -= delta
-		input = 'yaw right'
-	if Input.is_action_pressed("p1_yaw_left"):
-		yaw_dir += delta
-		input = 'yaw left'
-	if Input.is_action_pressed("p1_yaw_right"):
-		yaw_dir -= delta
-		input = 'yaw right'
+    pitch_dir = pitch_dir / (1 + delta)
+    roll_dir = roll_dir / (1 + delta)
+    if Input.is_action_pressed("p1_fire"):
+        $Plane.fire_weapon()
+        input = 'fire'
+    if Input.is_action_pressed("ui_up"):
+        thrust += delta
+        input = 'accel'
+    if Input.is_action_pressed("ui_down"):
+        thrust -= delta
+        input = 'slow'
+    if Input.is_action_pressed("p1_pitch_up"):
+        pitch_dir += delta
+        input = 'up'
+    if Input.is_action_pressed("p1_pitch_down"):
+        pitch_dir -= delta
+        input = 'down'
+    if Input.is_action_pressed("p1_roll_left"):
+        roll_dir += delta
+        input = 'yaw left'
+    if Input.is_action_pressed("p1_roll_right"):
+        roll_dir -= delta
+        input = 'yaw right'
+    if Input.is_action_pressed("p1_yaw_left"):
+        yaw_dir += delta
+        input = 'yaw left'
+    if Input.is_action_pressed("p1_yaw_right"):
+        yaw_dir -= delta
+        input = 'yaw right'
 
-	if Input.is_action_pressed("ui_quit"):
-		get_tree().quit()
-	
+    if Input.is_action_pressed("ui_quit"):
+        get_tree().quit()
+    
 #TODO Adjust values to make movement more smooth
 func process_movement(delta):
-	 thrust = clamp(thrust, 0.2, 1)
-	 pitch_dir  = clamp(pitch_dir, -.6, .6)
-	 yaw_dir  = clamp(yaw_dir, -.6, .6)
-	 roll_dir  = clamp(roll_dir, -.6, .6)
-	 $Plane.calc_force(thrust, pitch_dir, roll_dir, yaw_dir)
+     thrust = clamp(thrust, 0.2, 1)
+     pitch_dir  = clamp(pitch_dir, -.6, .6)
+     yaw_dir  = clamp(yaw_dir, -.6, .6)
+     roll_dir  = clamp(roll_dir, -.6, .6)
+     $Plane.calc_force(thrust, pitch_dir, roll_dir, yaw_dir)
